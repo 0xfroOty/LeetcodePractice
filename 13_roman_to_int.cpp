9df59eb5bbff17899fbc47a111090f63f3cbc821
0xfroOty <sathwikreddy962@gmail.com>
2026-03-23 17:24:22 +0530

    13-roman to integer

---
+++ 13_roman_to_int.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int value(char c) {
+  switch(c) {
+    case 'I' : return 1;
+    case 'V' : return 5;
+    case 'X' : return 10;
+    case 'L' : return 50;
+    case 'C' : return 100;
+    case 'D' : return 500;
+    case 'M' : return 1000;
+  }
+  return 0;
+}
+
+int main() {
+  string arr = "MCMXCIV";
+  int n = 7;
+
+  int total =0;
+  int previous = 0;
+
+  for(int i = n -1; i >= 0; i--) {
+    int current = value(arr[i]);
+
+    if(current < previous) {
+      total -= current;
+    } else {
+      total += current;
+    }
+    previous = current;
+  }
+
+  cout<<total;
+  return 0;
+}
